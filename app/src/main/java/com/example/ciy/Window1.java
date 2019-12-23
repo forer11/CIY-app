19207ab89a73ec039b25335d6113a4947c8d0e46
forer11 <lior.forer@cs.huji.ac.il>
2019-12-23 16:45:26 +0200

    added window 1

---
+++ app/src/main/java/com/example/ciy/Window1.java
@@ -0,0 +1,14 @@
+package com.example.ciy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Window1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_window1);
+    }
+}
