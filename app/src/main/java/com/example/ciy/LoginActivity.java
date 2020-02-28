6e19add098654a4d63977dff0e3db148c0526c76
shani shapp <shani.shapp@mail.huji.ac.il>
2020-02-28 21:35:52 +0200

    LoginActivity style changes
    added some drawables to be used in the layout.
    need to delete the action bar but at the overall the design in finished.

--- app/src/main/java/com/example/ciy/LoginActivity.java
+++ app/src/main/java/com/example/ciy/LoginActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import com.firebase.ui.auth.AuthMethodPickerLayout;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -54,11 +56,18 @@ public class LoginActivity extends BaseSignIn {
     this function shows all the sign in options
      */
     private void showOptions(){
+        AuthMethodPickerLayout customLayout = new AuthMethodPickerLayout
+                .Builder(R.layout.activity_new_signin)
+                .setGoogleButtonId(R.id.google_signIn)
+                .setEmailButtonId(R.id.email_signIn)
+                .setAnonymousButtonId(R.id.anonymous_signIn)
+                .setPhoneButtonId(R.id.phone_signIn)
+                .build();
         startActivityForResult(
                 AuthUI.getInstance().createSignInIntentBuilder()
                         .setAvailableProviders(providers)
-                        .setTheme(R.style.MyTheme)
                         .setIsSmartLockEnabled(true)
+                        .setAuthMethodPickerLayout(customLayout)
                         .build(), MY_REQUEST_CODE
         );
     }
