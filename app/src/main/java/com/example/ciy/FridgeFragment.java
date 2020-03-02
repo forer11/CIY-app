240829edb947e02c725fd4d217c2f5da0581eca1
forer11 <lior.forer@cs.huji.ac.il>
2020-03-02 23:04:09 +0200

    finished doc

--- app/src/main/java/com/example/ciy/FridgeFragment.java
+++ app/src/main/java/com/example/ciy/FridgeFragment.java
@@ -43,7 +43,7 @@ public class FridgeFragment extends DialogFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        return inflater.inflate(R.layout.fragment_fridge, container, false);
     }
 
     @Override
